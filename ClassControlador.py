153278d0f297dffa7b8911d4cd1436bfcdfea608
Sbastdia <sbastdia@myuax.com>
2021-10-22 17:19:11 +0200

    Creo que ya voy entendiendo

---
+++ ClassControlador.py
@@ -0,0 +1,19 @@
+from ClassVista import*
+from ClassModelo import*
+
+# El controlador hace el enlace entre la vista y el modelo,
+# realizando el procesamiento de datos.
+class Controlador:
+# El controlador pide a la vista que recupere una cadena
+# en la entrada estándar, y luego solicita al modelo
+# que la almacene.
+    def almacenarEntrada(self):
+        cadena = vista.entrada()
+        modelo.agregar(cadena.upper())
+    # El controlador recupera las cadenas del modelo
+    # y las escribirá en un archivo.
+    def guardarCadenas(self):
+        cadenas = modelo.recuperarCadenas()
+        with open('test.txt', 'w') as f:
+            for cadena in cadenas:
+                f.write(cadena)
